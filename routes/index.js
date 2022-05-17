61936c74f4185afaf9b0dc328f71e29ceefc8f86
alejandrolederman <alejandro.lederman@live.com.ar>
2022-05-17 14:13:19 -0300

    test2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Control de Acceso ' });
+  res.render('index', { title: 'esto es una prueba  ' });
 });
 
 module.exports = router;

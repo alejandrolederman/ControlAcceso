becb053b76ef0faf45cdfa9af31398d47a050109
alejandrolederman <alejandro.lederman@live.com.ar>
2022-05-24 09:37:58 -0300

    reparacion?

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var alumnosController = require("../controllers/alumnosController")
+
+// creacion
+
+//  router.get("/crear", alumnosController.crear);
+
+//  router.post("/crear", alumnosController.guardado);
+
+ //listado
+//  router.get("/", alumnosController.listado);
+
+ //detalle
+
+//  router.get("/:id", alumnosController.detalle);
+
+ //editar
+
+//  router.get("/editar/:id", alumnosController.editar);
+
+//  router.post("/editar/:id", alumnosController.actualizar);
+
+ //borrar
+//  router.post("/borrar/:id", alumnosController.borrar);
+
+
+
+module.exports = router;
